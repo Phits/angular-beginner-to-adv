20885df40a44f641370e51097e67f05f9223c5fc
phits <john@fitz.com>
2018-07-26 15:25:10 -0400

    Cleaned up code

--- hello-world/src/app/favorite/favorite.component.ts
+++ hello-world/src/app/favorite/favorite.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-import { faStar } from '@fortawesome/free-solid-svg-icons';
-
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
