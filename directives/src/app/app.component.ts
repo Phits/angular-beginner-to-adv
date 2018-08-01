fbfb3b1ce44b30d7d477a3d0368e0f4ed983d459
phits <john@fitz.com>
2018-08-01 16:32:18 -0400

    Almost done with section 6

---
+++ directives/src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  task = {
+    title: 'Review applications',
+    assignee: {
+      name: 'John Fitz'
+    }
+  }
+  canSave = true;
+  viewMode = '';
+  title = 'Directives';
+  courses;
+
+  trackCourse(index, course) {
+    return course ? course.id : undefined;
+  }
+
+  loadCourses() {
+    this.courses =[
+      { id: 1, name: 'Course 1' },
+      { id: 2, name: 'Course 2' },
+      { id: 3, name: 'Course 3' }
+    ];
+  }
+
+  onAdd() {
+    this.courses.push({ id: 4, name: 'Course 4' });
+  }
+
+  onChange(course) {
+    course.name = 'Updated Name'
+  }
+
+  onRemove(course) {
+    let index = this.courses.indexOf(course);
+    this.courses.splice(index, 1);
+  }
+}
