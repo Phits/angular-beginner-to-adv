c2b252310cbc446ef745e32542902ab042bf1152
phits <john@fitz.com>
2018-08-14 13:38:55 -0400

    onInit Interface

--- consume-http-services/src/app/posts/posts.component.ts
+++ consume-http-services/src/app/posts/posts.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
 
 @Component({
@@ -6,14 +6,18 @@ import { Http } from '@angular/http';
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
-export class PostsComponent {
+export class PostsComponent implements OnInit {
   posts: any[];
   private url = 'http://jsonplaceholder.typicode.com/posts';
 
   constructor(private http: Http) {
-      http.get(this.url).subscribe(response => {
-       this.posts = response.json();
-      })
+     
+  }
+
+  ngOnInit() {
+    this.http.get(this.url).subscribe(response => {
+      this.posts = response.json();
+     })
   }
 
   createPost(input: HTMLInputElement) {
@@ -25,6 +29,20 @@ export class PostsComponent {
       this.posts.splice(0, 0, post);
       console.log(response.json());
     });
+  }
+
+  updatePost(post) {
+     // this.http.put(this.url, JSON.stringify(post));
+    this.http.patch(this.url + '/' + post.id, JSON.stringify({ isRead: true })).subscribe(response => {
+      console.log(response.json());
+    })
+  }
+
+  deletePost(post) {
+    this.http.delete(this.url + '/' + post.id).subscribe(response => {
+      let index = this.posts.indexOf(post);
+      this.posts.splice(index, 1);
+    })
   }
 
 }
