7a6f8943eccf0ba0e8cd5e19059278561b0b68fc
phits <john@fitz.com>
2018-08-14 16:56:18 -0400

    Handling errors

--- consume-http-services/src/app/services/post.service.ts
+++ consume-http-services/src/app/services/post.service.ts
@@ -8,23 +8,21 @@ import { stringify } from '@angular/compiler/src/util';
 export class PostService {
   private url = 'http://jsonplaceholder.typicode.com/posts';
 
-  constructor(private http: Http) { 
-
-  }
+  constructor(private http: Http) {}
 
   getPosts() {
    return this.http.get(this.url);
   }
 
   createPost(post) {
-    return this.http.post(this.url, JSON,stringify(post));
+    return this.http.post(this.url, JSON.stringify(post));
   }
 
   updatePost(post) {
     return  this.http.patch(this.url + '/' + post.id, JSON.stringify({ isRead: true }));
   }
 
-  deletePost(post) {
-    return this.http.delete(this.url + '/' + post.id);
+  deletePost(id) {
+    return this.http.delete(this.url + '/' + id);
   }
 }
