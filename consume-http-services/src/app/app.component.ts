621e9f0e52fd1f91a997a314ee251730ca5dd8f1
phits <john@fitz.com>
2018-08-14 11:13:58 -0400

    Working on posting data

--- consume-http-services/src/app/app.component.ts
+++ consume-http-services/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PostsComponent } from './posts/posts.component'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Consumming HTTP Services';
 }
