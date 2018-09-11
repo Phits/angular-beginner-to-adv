fefa5684be1b516ba959c4114caa31033f0d54fd
phits <john@fitz.com>
2018-09-11 16:09:07 -0400

    Reading an object from firebase

--- firebase-demo/src/app/app.component.ts
+++ firebase-demo/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnDestroy } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs';
+import { FirebaseDatabase, FirebaseStorage } from 'angularfire2';
 // import { Subscription } from 'rxjs';
 
 @Component({
@@ -10,18 +12,29 @@ import { AngularFireDatabase } from 'angularfire2/database';
 export class AppComponent {
   title = 'Firebase Demo';
   courses$;
+  course$;
+  author$;
+
   // courses: any;
   // subscription: Subscription;
 
   constructor(db: AngularFireDatabase) {
 
     this.courses$ = db.list('/courses').valueChanges();
+    this.course$ = db.object('/courses/1').valueChanges();
+    this.author$ = db.object('/authors/1').valueChanges();
+    // console.log(this.author$);
 
     // this.subscription = db.list('/courses').valueChanges().subscribe((courses) => {
     //   this.courses = courses;
     //   console.log(this.courses);
     //  });
 
+  }
+
+  add(course: HTMLInputElement) {
+    this.course$.set(course.value);
+    course.value = '';
   }
 
   // ngOnDestroy() {
