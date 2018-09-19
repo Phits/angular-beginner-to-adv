16bf35f15c0e9a044f06082091bf1cb3759d87af
phits <john@fitz.com>
2018-09-19 16:30:25 -0400

    good progress on angular material

--- material-demo/src/app/app.component.ts
+++ material-demo/src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Material Demo';
+  isChecked = true;
+  colors = [
+    {id: 1, name: 'Red'},
+    {id: 2, name: 'Green'},
+    {id: 3, name: 'Blue'}
+  ];
+
+  color = 1;
+
+  minDate = new Date(2018, 1, 1);
+  maxDate = new Date(2019, 1, 1);
+
+  onChange($event) {
+    console.log($event);
+  }
+
 }
--- material-demo/src/app/app.module.ts
+++ material-demo/src/app/app.module.ts
@@ -3,8 +3,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 // import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [
@@ -13,7 +19,13 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatRadioModule,
+    MatSelectModule,
+    FormsModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
