62e3e30e97c0f0f737293b888e1a2c9261cba378
phits <john@fitz.com>
2018-09-21 16:39:07 -0400

    Excellent progress on angular material

--- material-demo/src/app/app.component.ts
+++ material-demo/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable, timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,18 +7,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  progress = 0;
+  timer;
+  isLoading = false;
+
+  constructor() {
+    this.isLoading = true;
+    this.getCourses()
+    .subscribe(x => this.isLoading = false);
+
+    this.timer = setInterval(() => {
+     this.progress++;
+     if (this.progress === 100) {clearInterval(this.timer)}
+    }, 20);
+  }
+
+  categories = [
+    { name: 'Beginner' },
+    { name: 'Intermediate' },
+    { name: 'Advanced' }
+  ];
+
   title = 'Material Demo';
   isChecked = true;
   colors = [
-    {id: 1, name: 'Red'},
-    {id: 2, name: 'Green'},
-    {id: 3, name: 'Blue'}
+    { id: 1, name: 'Red' },
+    { id: 2, name: 'Green' },
+    { id: 3, name: 'Blue' }
   ];
 
   color = 1;
 
   minDate = new Date(2018, 1, 1);
   maxDate = new Date(2019, 1, 1);
+
+  getCourses() {
+    return timer(2000);
+  }
+
+  selectCategory(category) {
+    this.categories
+    .filter(c => c !== category)
+    .forEach(c => c['selected'] = false);
+
+    category.selected = !category.selected;
+  }
 
   onChange($event) {
     console.log($event);
--- material-demo/src/app/app.module.ts
+++ material-demo/src/app/app.module.ts
@@ -7,10 +7,15 @@ import {MatRadioModule} from '@angular/material/radio';
 import {MatSelectModule} from '@angular/material/select';
 import {MatInputModule} from '@angular/material/input';
 import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { MatNativeDateModule } from '@angular/material/core';
+
 
 @NgModule({
   declarations: [
@@ -25,7 +30,11 @@ import { MatNativeDateModule } from '@angular/material/core';
     FormsModule,
     MatInputModule,
     MatDatepickerModule,
-    MatNativeDateModule
+    MatNativeDateModule,
+    MatIconModule,
+    MatButtonModule,
+    MatChipsModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
