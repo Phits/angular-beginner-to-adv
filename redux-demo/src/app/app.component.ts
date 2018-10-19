32949ea6c3518807d00958a818b4a6fee4396eb6
phits <john@fitz.com>
2018-10-19 16:48:25 -0400

    Redux counter working

--- redux-demo/src/app/app.component.ts
+++ redux-demo/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NgRedux } from '@angular-redux/store';
+import { IAppState } from './store';
+import { INCREMENT } from './actions';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'redux-demo';
+  counter = 0;
+
+  constructor(private ngRedux: NgRedux<IAppState>) {
+     ngRedux.subscribe(() => {
+       var store = ngRedux.getState();
+       this.counter = store.counter;
+     });
+  }
+
+  increment() {
+    // console.log('Increment');
+    this.ngRedux.dispatch( {type: INCREMENT});
+  }
 }
--- redux-demo/src/app/app.module.ts
+++ redux-demo/src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgRedux, NgReduxModule } from '@angular-redux/store';
-import { IAppState, rootReducer } from './store';
 
 import { AppComponent } from './app.component';
+import { IAppState, rootReducer, INTIAL_STATE } from './store';
 
 @NgModule({
   declarations: [
@@ -18,6 +18,6 @@ import { AppComponent } from './app.component';
 })
 export class AppModule {
   constructor(ngRedux: NgRedux<IAppState>) {
-     ngRedux.configureStore(rootReducer, {});
+     ngRedux.configureStore(rootReducer, INTIAL_STATE);
   }
  }
