4ad78209588a744880551ae611c1560b7035c60f
phits <john@fitz.com>
2018-11-06 16:52:42 -0500

    Good start on testing

--- testing-demo/src/app/03-setup-and-teardown/vote.component.spec.ts
+++ testing-demo/src/app/03-setup-and-teardown/vote.component.spec.ts
@@ -1,9 +1,23 @@
 import { VoteComponent } from './vote.component'; 
 
 describe('VoteComponent', () => {
-  it('', () => {
+  let component: VoteComponent;
+
+  beforeEach(() => {
+      component = new VoteComponent();
   });
 
-  it('', () => {
+  it('should increment totalVotes when upvotes', () => {
+    component.upVote();
+
+    expect(component.totalVotes).toBe(1);
   });
+
+  it('should drecrement totalVotes when downvotes', () => {
+    component.downVote();
+
+    expect(component.totalVotes).toBe(-1);
+  });
+
+
 });
