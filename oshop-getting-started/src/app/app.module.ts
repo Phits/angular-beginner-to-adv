3f8f69e8a4df7a7b28340a8d4b6ab39bf86e198b
phits <john@fitz.com>
2018-11-15 16:49:13 -0500

    Working on routing

--- oshop-getting-started/src/app/app.module.ts
+++ oshop-getting-started/src/app/app.module.ts
@@ -6,6 +6,12 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { NavComponent } from './nav/nav.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { CheckOutComponent } from './check-out/check-out.component';
+import { OrderSuccessComponent } from './order-success/order-success.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
 
 const APP_ID = 'oshop-getting-started';
 
@@ -14,7 +20,13 @@ const APP_ID = 'oshop-getting-started';
     AppComponent,
     NavComponent,
     ShoppingCartComponent,
-    HomeComponent
+    HomeComponent,
+    ProductsComponent,
+    CheckOutComponent,
+    OrderSuccessComponent,
+    MyOrdersComponent,
+    AdminProductsComponent,
+    AdminOrdersComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: APP_ID }),
