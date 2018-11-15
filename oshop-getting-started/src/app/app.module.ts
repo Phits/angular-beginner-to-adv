0eb365fd1db9896cd3de709c48406e8783b4035c
phits <john@fitz.com>
2018-11-15 11:18:52 -0500

    Updated bootstrap nav bar

--- oshop-getting-started/src/app/app.module.ts
+++ oshop-getting-started/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BsDropdownModule } from 'ngx-bootstrap';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { NavComponent } from './nav/nav.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { HomeComponent } from './home/home.component';
@@ -19,7 +19,7 @@ const APP_ID = 'oshop-getting-started';
   imports: [
     BrowserModule.withServerTransition({ appId: APP_ID }),
     AppRoutingModule,
-    BsDropdownModule.forRoot()
+    NgbModule
   ],
   providers: [],
   bootstrap: [
